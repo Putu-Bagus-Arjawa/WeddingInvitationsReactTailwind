f57c9711a94a412839e7a8e2a963e229481b050c
Arjawa <arjawabagus14@gmail.com>
2025-05-05 06:06:56 +0800

    add animation in marriage section

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,21 +1,51 @@
-
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react";
 
 const Hero = () => {
     const time = [4, 9, 12, 5, 27, 0]
+    const refer = useRef<HTMLDivElement>(null);
+    const isInView = useInView(refer, { once: false });
   return (
-    <section className="min-h-screen flex justify-center" id="home">
+    <motion.section 
+        className="min-h-screen flex justify-center" id="home"
+        ref={refer}
+    >
         <div className="bg-linear-to-r from-black/0 via-black/40 to-black/0 min-h-screen w-full flex justify-center items-center gap-y-8 flex-col">
             <div className="flex flex-col justify-center items-center">
-                <h1 className="font-playfair text-5xl text-white tracking-widest">The Marriage of</h1>
-                <h2 className="text-3xl  text-white tracking-wide italic font-playfair">ARKA & PUTRI</h2>
+                <motion.h1 
+                    className="font-playfair text-5xl text-white tracking-widest"
+                    initial ={{ opacity: 0, x :-500 }}
+                    animate ={isInView?{ opacity: 1, x :0 }: {}}
+                    transition={{duration: 1, ease:"easeInOut"}}
+                >The Marriage of</motion.h1>
+                <motion.h2 
+                    className="text-3xl  text-white tracking-wide italic font-playfair"
+                    initial ={{ opacity: 0, x :500 }}
+                    animate ={isInView?{ opacity: 1, x :0 }: {}}
+                    transition={{duration: 1, ease:"easeInOut"}}
+                >ARKA & PUTRI</motion.h2>
             </div>
             <div className="flex gap-4">
-                {time.map((item)=> (
-                    <div className="bg-white rounded-xl w-12 h-12 flex items-center justify-center font-playfair">{item}</div>
+                {time.map((item, idx)=> (
+                    <motion.div 
+                        key={idx}
+                        className="bg-white rounded-xl w-12 h-12 flex items-center justify-center font-playfair"
+                        initial = {{ opacity: 0, y: 200 }}
+                        animate ={isInView? "visible" : "hidden"}
+                        variants={{
+                            hidden:{ opacity: 0, y: 200 },
+                            visible:{ opacity: 1, y: 0, transition: {
+                                delay: 0.7 + (idx * 0.2),
+                                type: "spring",
+                                bounce: 0.6
+                                }
+                            },
+                        }}
+                    >{item}</motion.div>
                 ))}
             </div>
         </div>
-    </section>
+    </motion.section>
   )
 }
 
--- src/pages/Marriage.tsx
+++ src/pages/Marriage.tsx
@@ -2,28 +2,44 @@ import Card from "../components/Card"
 import Quotes from "../components/Quotes"
 import male from "../assets/male.png"
 import female from "../assets/female.png"
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react"
 
 const Marriage = () => {
     const couple = [
         {name: "Arka Bumantara Radjawa", kid: "First Son of Anjaya Budiantara & Ratna Rina", image:male},
         {name: "Putri Luna Djelita", kid: "Second daughter of Bramantya Bumantara & Serliyana Djelita", image:female}
     ]
+        const refer = useRef<HTMLDivElement>(null);
+        const isInView = useInView(refer, { once: false });
+    
   return (
-    <section className="md:min-h-screen h-auto" id="couple">
+    <section className="md:min-h-screen h-auto" id="couple" ref={refer}>
       <Quotes/>
       <div className="flex flex-col items-center md:gap-y-4 gap-y-8">
-        <h1 className="text-rose-600 mt-8 text-4xl font-playfair">The Couple</h1>
+        <motion.h1 className="text-rose-600 mt-8 text-4xl font-playfair" 
+          initial ={{ opacity: 0, x :-500, rotate:0 }}
+          animate ={isInView?{ opacity: 1, x :0, rotate: 360 }: {}}
+          transition={{duration: 0.7, ease:"easeInOut"}}
+        >The Couple</motion.h1>
         <p className="p-[2px] bg-rose-500 w-14"></p>
       </div>
       <section className="p-8 md:min-h-screen h-auto flex md:flex-row flex-col justify-center gap-x-20 items-center gap-y-8">
-        {couple.map((items)=>(
-            <Card>
+        {couple.map((items, idx)=>(
+          <motion.div
+            key={idx}
+            initial ={idx%2 == 0?{ opacity: 0, y:-500 }:{opacity:0, y: 500}}
+            animate ={isInView?{ opacity: 1, y :0 }: {}}
+            transition={{type:"spring", duration: 1.2, bounce:0.4, ease:"easeInOut"}}
+          >
+              <Card>
                 <div className="flex flex-col items-center max-w-80">
                     <img src={items.image} alt="" className="h-50 w-50 mb-8 rounded-full" />
                     <h1 className="font-playfair text-2xl text-rose-500">{items.name}</h1>
                     <p className="text-neutral-500 font-quickSand text-center">{items.kid}</p>
                 </div>
             </Card>
+          </motion.div>
         ))}
       </section>
     </section>
