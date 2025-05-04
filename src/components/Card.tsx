76ef2b0028f16d5d0712854370f174572109ae33
Arjawa <arjawabagus14@gmail.com>
2025-05-05 07:18:54 +0800

    try to fix card cant scale when touch in mobile

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import Child from "./Child"
 
 const Card = ({children}:Child) => {
   return (
-    <div className="p-8 shadow-xl bg-white/75 hover:scale-105 hover:shadow-lg">
+    <div className="p-8 shadow-xl bg-white/75 hover:scale-105 active:scale-105  hover:shadow-lg">
       {children}
     </div>
   )
