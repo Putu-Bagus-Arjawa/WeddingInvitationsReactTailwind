5e50a7c28c653882a825c71c2c1192b5fac8994e
Arjawa <arjawabagus14@gmail.com>
2025-05-05 05:34:50 +0800

    give animation in quotes component

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,10 +1,29 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Quotes = () => {
+  const refer = useRef<HTMLDivElement>(null);
+  const isInView = useInView(refer, { once: false });
+  
   return (
-    <section className="flex  justify-center items-center p-12 font-quickSand  text-2xl tracking-wide bg-linear-to-l from-rose-500 to-fuchsia">
-        <p className="italic"><span>"</span>The World Might End One Day, But Their Love Will Remain Forever<span>"</span></p>
-    </section>
+    <motion.section 
+      className="flex justify-center items-center p-12 font-quickSand text-2xl tracking-wide bg-linear-to-l from-rose-500 to-fuchsia"
+      ref={refer}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={isInView ? { opacity: 1, scale: 1 } : {}}
+      transition={{ duration: 0.5 }}
+    >
+      <p className="italic">
+        <span>"</span>
+        
+        <span >
+          The World Might End One Day, But Their Love Will Remain Forever
+        </span>
+        
+        <span>"</span>
+      </p>
+    </motion.section>
   )
 }
 
-export default Quotes
+export default Quotes;
